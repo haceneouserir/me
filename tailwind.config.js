d18cbcd807962b1b650f9856989d330c03aba552
Hacene Ouserir <haceneouserir@gmail.com>
2025-09-04 21:09:20 +0100

    Fix sidebar size in xs media

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ export default {
   content: ["./**/*.{php,html,js}", "!./**/node_modules/**/*.{html,js}"],
   theme: {
     extend: {
+      screens: {
+        'xs-max': { 'max': '375px'},
+      },
       colors: {
         capri: "#00bfff",
         "capri-light": "#33ccff",
@@ -11,7 +14,11 @@ export default {
       animation: {
         "bounce-slow": "bounce 3s infinite;",
       },
+      width: {
+        68: "17rem",
+      },
       inset: {
+        68: "17rem",
         80: "20rem",
       },
       margin: {
